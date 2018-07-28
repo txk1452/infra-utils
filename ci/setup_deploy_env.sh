63e5a62fccf661c5880453145daad7bd261d5a7e
Khai Do <zaro0508@gmail.com>
2018-07-27 18:11:08 -0700

    move --exists-action option to command

--- ci/setup_deploy_env.sh
+++ ci/setup_deploy_env.sh
@@ -1,8 +1,9 @@
+# This script assumes that you already have the awscli installed and
+# profiles setup for using the cli
 #!/bin/bash
 set -e
 
 wget https://github.com/Sage-Bionetworks/infra-utils/archive/master.zip -O /tmp/infra-utils.zip
 unzip -j -n /tmp/infra-utils.zip -x "infra-utils-master/.gitignore" "infra-utils-master/LICENSE" "infra-utils-master/*.md" "infra-utils-master/aws/*"
-./setup_aws_cli.sh --exists-action a || travis_terminate 1
-./setup_sceptre.sh --exists-action a|| travis_terminate 1
+./setup_sceptre.sh || travis_terminate 1
 
