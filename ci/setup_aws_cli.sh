a093ba74296766d24215e87c2ab17b16c3bad074
txk1452 <106824651@163.com>
2018-09-05 16:53:50 +0800

    Update setup_aws_cli.sh

--- ci/setup_aws_cli.sh
+++ ci/setup_aws_cli.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# double interpolate vars when using multiple branches
-
-eval export "AwsCfServiceRoleArn=$RoleArn"
-eval export "AwsTravisAccessKey=$AccessKey"
-eval export "AwsTravisSecretAccessKey=$SecretAccessKey"
-
 pip install --upgrade awscli
 mkdir ~/.aws
 echo -e "[default]\nregion=us-east-1\nsource_profile=default\nrole_arn=$AwsCfServiceRoleArn" > ~/.aws/config
