5bbff50607a79241d092c957d8754ffc172a0d49
txk1452 <106824651@163.com>
2018-09-05 20:36:27 +0800

    Update setup_sceptre.sh

--- ci/setup_sceptre.sh
+++ ci/setup_sceptre.sh
@@ -5,5 +5,5 @@ set -e
 pip install --exists-action a "sceptre>=1.3"
 # install contributions
 mkdir resolvers
-wget https://raw.githubusercontent.com/cloudreach/sceptre/v1/contrib/ssm-resolver/ssm.py -O resolvers/ssm.py
-wget https://raw.githubusercontent.com/cloudreach/sceptre/v1/contrib/kms-resolver/kms.py -O resolvers/kms.py
+wget https://raw.githubusercontent.com/txk1452/sceptre/v1/contrib/ssm-resolver/ssm.py -O resolvers/ssm.py
+wget https://raw.githubusercontent.com/txk1452/sceptre/v1/contrib/kms-resolver/kms.py -O resolvers/kms.py
