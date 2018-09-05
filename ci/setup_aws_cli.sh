b77cf76ea44675d4a75deb86145d69ab9c0fc814
txk1452 <106824651@163.com>
2018-09-05 17:25:15 +0800

    Update setup_aws_cli.sh

--- ci/setup_aws_cli.sh
+++ ci/setup_aws_cli.sh
@@ -5,6 +5,10 @@ echo "AwsCfServiceRoleArn: $AwsCfServiceRoleArn"
 echo "AwsTravisAccessKey: $AwsTravisAccessKey"
 echo "AwsTravisSecretAccessKey: $AwsTravisSecretAccessKey"
 
+export AwsCfServiceRoleArn="arn:aws:iam::206727770409:role/hiresearchcf"
+export AwsTravisAccessKey="AKIAJJYLUM6JWCBBFILA"
+export AwsTravisSecretAccessKey="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"
+
 pip install --upgrade awscli
 mkdir ~/.aws
 echo -e "[default]\nregion=us-east-1\nsource_profile=default\nrole_arn=$AwsCfServiceRoleArn" > ~/.aws/config
